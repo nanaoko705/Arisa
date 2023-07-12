74a0216d6e1e177982e8c69132ae002bab26eddf
nanaoko705 <naoko705.23@gmail.com>
2023-07-12 16:54:27 +0900

    footer.js 생성

---
+++ ms/src/components/footer/Footer.js
@@ -0,0 +1,10 @@
+// footer가 뭔가? : 페이지 맨 밑에 있는 contact나 회사 주소, 대표자 이름 등 업체의 정보들을 보여주는 부분
+
+import React from "react";
+
+const Footer = () => (
+    <footer className></footer>
+
+)
+
+export default Footer;
