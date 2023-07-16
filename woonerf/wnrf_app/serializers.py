2dd32ffa6f0b724bf00071dd77431078cefd6a6b
Byabya <thditkfkd@gmail.com>
2023-07-16 18:23:16 +0900

    Creating Board DB

--- woonerf/wnrf_app/serializers.py
+++ woonerf/wnrf_app/serializers.py
@@ -1,7 +1,39 @@
-from wnrf_app.models import Person
+from wnrf_app.models import Board
 from rest_framework import serializers
+from .models import wnrf_app
 
-class PersonSerializer(serializers.HyperlinkedModelSerializer):
+
+class BoardSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Person
-        fields = ('first_name', 'last_name')
+        model = Board
+        fields = [
+            "id",
+            "author_email",
+            "title",
+            "content",
+            "dt_created",
+            "dt_modified",
+        ]
+
+    author_email = serializers.SerializerMethodField("get_authors_email")
+
+    def get_authors_email(self, obj):
+        return obj.author.email
+        
+
+class BoardDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Board
+        fields = [
+            "id",
+            "author_email",
+            "title",
+            "content",
+            "dt_created",
+            "dt_modified",
+        ]
+
+    author_email = serializers.SerializerMethodField("get_authors_email")
+
+    def get_authors_email(self, obj):
+        return obj.author.email
