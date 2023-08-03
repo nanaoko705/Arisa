4f3c4095cd491d3cc57a78507219d3417ff7b37c
nanaoko705 <naoko705.23@gmail.com>
2023-08-04 01:13:21 +0900

    로고 왼쪽 정렬 성공

--- woonerf/react-woonerf/src/App.js
+++ woonerf/react-woonerf/src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import React from "react";
-import styled from 'styled-components'
 
 import BlogList from "./components/BlogList.js";
 import MainLogo from "./components/mainLogo/MainLogo";
@@ -8,19 +7,10 @@ import Nav from "./components/nav/Nav";
 
 const menus = ['About', 'Class', 'Shop', 'Archive']
 
-const Circle = styled.div`
-  width: 5rem;
-  height: 5rem;
-  background: black;
-  border-radius: 50%;
-`;
-
-
 function App() {
   return (
     <div className="App">
-      {/* <MainLogo /> */}
-      <Circle />
+      <MainLogo />
       <header className="App-header">
         <Nav menu={menus}/>
       </header>
--- woonerf/react-woonerf/src/components/nav/Nav.js
+++ woonerf/react-woonerf/src/components/nav/Nav.js
@@ -14,11 +14,12 @@ const StyledNav = styled.button`
     padding-right: 1rem;
 
     /* 크기 */
+    width: 10rem;
     height: 2.25rem;
     font-size: 1rem;
 
     /* 색상 */
-    background: #228be6;
+    background: rgba(0,0,0,0);
     &:hover {
     background: #339af0;
     }
@@ -35,34 +36,19 @@ const StyledNav = styled.button`
 const Nav = (props) => (
     <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
         <div className="container">
-            {/* 타이틀도 그림으로 바꾸자 */}
-            {/* <a className="navbar-brand js-scroll-trigger" href="#page-top" >Woonerf</a> */}
-            <div classNIame="MainLogo">
-                <img src="http://127.0.0.1:8000/media/MainLogo/wnrf_green_gold-removebg-preview.png" alt="메인 로고입니다. 클릭하면 홈으로 돌아갑니다." /> 
-            </div>
-            
-            {/* 일단 메뉴버튼 지우기 */}
-            {/* <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
-                data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
-                aria-label="Toggle navigation">
-            Menu
-            <i className="fas fa-bars"></i>
-            </button> */} 
             <div className="collapse navbar-collapse" id="navbarResponsive">
-                <ul className="navbar-nav ml-auto">
-                    <button className="nav-item">
-                        <a className="nav-link js-scroll-trigger" href="#about">{props.menu[0]}</a>
-                    </button>
-                    <button className="nav-item">
-                        <a className="nav-link js-scroll-trigger" href="#news">{props.menu[1]}</a>
-                    </button>
-                    <button className="nav-item">
-                        <a className="nav-link js-scroll-trigger" href="#gallery">{props.menu[2]}</a>
-                    </button>
-                    <button className="nav-item">
-                        <a className="nav-link js-scroll-trigger" href="#shop">{props.menu[3]}</a>
-                    </button>
-                </ul>
+                <StyledNav className="nav-item">
+                    <a className="nav-link js-scroll-trigger" href="#about">{props.menu[0]}</a>
+                </StyledNav>
+                <StyledNav className="nav-item">
+                    <a className="nav-link js-scroll-trigger" href="#class">{props.menu[1]}</a>
+                </StyledNav>
+                <StyledNav className="nav-item">
+                    <a className="nav-link js-scroll-trigger" href="#shop">{props.menu[2]}</a>
+                </StyledNav>
+                <StyledNav className="nav-item">
+                    <a className="nav-link js-scroll-trigger" href="#archive">{props.menu[3]}</a>
+                </StyledNav>
             </div>
         </div>
     </nav>
