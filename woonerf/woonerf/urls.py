bcc041c36f483297d31877bf82841a812ccc240a
nanaoko705 <naoko705.23@gmail.com>
2023-09-16 12:00:07 +0900

    urls.py?

--- woonerf/woonerf/urls.py
+++ woonerf/woonerf/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("user/", include('account.urls')),  # Comma was missing here
     #path("", include('board.urls')),
-    path('', TemplateView.as_view(template_name='index.html')),
+    path('', TemplateView.as_view(template_name='index.html')), #??
 
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
